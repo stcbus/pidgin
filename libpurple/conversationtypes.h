c7ea8c87b8a1b2ee49041400817942a8a053e7cb
Gary Kramlich <grim@reaperworld.com>
2020-08-13 18:29:17 -0500

    Move PurpleIMConversation to its own file
    Move PurpleIMConversation to it's own file.
    
    Testing Done:
    Compiled and was able to send an IM.  It's not yet using `G_DECLARE_FINAL_TYPE` because `PurpleConversation` is not using `G_DECLARE_*` so it fails if I try to use it.  I didn't clean up too much of the code yet, but I tried to get most of the formatting changes we have.
    
    Reviewed at https://reviews.imfreedom.org/r/60/

--- libpurple/conversationtypes.h
+++ libpurple/conversationtypes.h
@@ -25,26 +25,17 @@
 
 #ifndef PURPLE_CONVERSATION_TYPES_H
 #define PURPLE_CONVERSATION_TYPES_H
+
 /**
  * SECTION:conversationtypes
  * @section_id: libpurple-conversationtypes
  * @short_description: <filename>conversationtypes.h</filename>
- * @title: Chat and IM Conversation Objects
+ * @title: Chat Conversation Objects
  */
 
 /**************************************************************************/
 /* Data Structures                                                        */
 /**************************************************************************/
-
-#define PURPLE_TYPE_IM_CONVERSATION              (purple_im_conversation_get_type())
-#define PURPLE_IM_CONVERSATION(obj)              (G_TYPE_CHECK_INSTANCE_CAST((obj), PURPLE_TYPE_IM_CONVERSATION, PurpleIMConversation))
-#define PURPLE_IM_CONVERSATION_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST((klass), PURPLE_TYPE_IM_CONVERSATION, PurpleIMConversationClass))
-#define PURPLE_IS_IM_CONVERSATION(obj)           (G_TYPE_CHECK_INSTANCE_TYPE((obj), PURPLE_TYPE_IM_CONVERSATION))
-#define PURPLE_IS_IM_CONVERSATION_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE((klass), PURPLE_TYPE_IM_CONVERSATION))
-#define PURPLE_IM_CONVERSATION_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), PURPLE_TYPE_IM_CONVERSATION, PurpleIMConversationClass))
-
-typedef struct _PurpleIMConversation         PurpleIMConversation;
-typedef struct _PurpleIMConversationClass    PurpleIMConversationClass;
 
 #define PURPLE_TYPE_CHAT_CONVERSATION            (purple_chat_conversation_get_type())
 #define PURPLE_CHAT_CONVERSATION(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), PURPLE_TYPE_CHAT_CONVERSATION, PurpleChatConversation))
@@ -57,22 +48,6 @@ typedef struct _PurpleChatConversation       PurpleChatConversation;
 typedef struct _PurpleChatConversationClass  PurpleChatConversationClass;
 
 typedef struct _PurpleChatUser       PurpleChatUser;
-
-/**
- * PurpleIMTypingState:
- * @PURPLE_IM_NOT_TYPING: Not typing.
- * @PURPLE_IM_TYPING:     Currently typing.
- * @PURPLE_IM_TYPED:      Stopped typing momentarily.
- *
- * The typing state of a user.
- */
-typedef enum
-{
-	PURPLE_IM_NOT_TYPING = 0,
-	PURPLE_IM_TYPING,
-	PURPLE_IM_TYPED
-
-} PurpleIMTypingState;
 
 /**
  * PurpleChatUserFlags:
@@ -98,35 +73,8 @@ typedef enum /*< flags >*/
 
 } PurpleChatUserFlags;
 
+#include "purpleimconversation.h"
 #include "conversation.h"
-
-/**************************************************************************/
-/* PurpleIMConversation                                                   */
-/**************************************************************************/
-/**
- * PurpleIMConversation:
- *
- * Structure representing an IM conversation instance.
- */
-struct _PurpleIMConversation
-{
-	PurpleConversation parent_object;
-};
-
-/**
- * PurpleIMConversationClass:
- *
- * Base class for all #PurpleIMConversation's
- */
-struct _PurpleIMConversationClass {
-	PurpleConversationClass parent_class;
-
-	/*< private >*/
-	void (*_purple_reserved1)(void);
-	void (*_purple_reserved2)(void);
-	void (*_purple_reserved3)(void);
-	void (*_purple_reserved4)(void);
-};
 
 /**************************************************************************/
 /* PurpleChatConversation                                                 */
@@ -157,160 +105,6 @@ struct _PurpleChatConversationClass {
 };
 
 G_BEGIN_DECLS
-
-/**************************************************************************/
-/* IM Conversation API                                                    */
-/**************************************************************************/
-
-/**
- * purple_im_conversation_get_type:
- *
- * Returns: The #GType for the IMConversation object.
- */
-GType purple_im_conversation_get_type(void);
-
-/**
- * purple_im_conversation_new:
- * @account: The account opening the conversation window on the purple
- *                user's end.
- * @name:    Name of the buddy.
- *
- * Creates a new IM conversation.
- *
- * Returns: The new conversation.
- */
-PurpleIMConversation *purple_im_conversation_new(PurpleAccount *account,
-		const char *name);
-
-/**
- * purple_im_conversation_set_icon:
- * @im:   The IM.
- * @icon: The buddy icon.
- *
- * Sets the IM's buddy icon.
- *
- * This should only be called from within Purple. You probably want to
- * call purple_buddy_icon_set_data().
- *
- * See purple_buddy_icon_set_data().
- */
-void purple_im_conversation_set_icon(PurpleIMConversation *im, PurpleBuddyIcon *icon);
-
-/**
- * purple_im_conversation_get_icon:
- * @im: The IM.
- *
- * Returns the IM's buddy icon.
- *
- * Returns: The buddy icon.
- */
-PurpleBuddyIcon *purple_im_conversation_get_icon(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_set_typing_state:
- * @im:    The IM.
- * @state: The typing state.
- *
- * Sets the IM's typing state.
- */
-void purple_im_conversation_set_typing_state(PurpleIMConversation *im, PurpleIMTypingState state);
-
-/**
- * purple_im_conversation_get_typing_state:
- * @im: The IM.
- *
- * Returns the IM's typing state.
- *
- * Returns: The IM's typing state.
- */
-PurpleIMTypingState purple_im_conversation_get_typing_state(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_start_typing_timeout:
- * @im:      The IM.
- * @timeout: How long in seconds to wait before setting the typing state
- *        to PURPLE_IM_NOT_TYPING.
- *
- * Starts the IM's typing timeout.
- */
-void purple_im_conversation_start_typing_timeout(PurpleIMConversation *im, int timeout);
-
-/**
- * purple_im_conversation_stop_typing_timeout:
- * @im: The IM.
- *
- * Stops the IM's typing timeout.
- */
-void purple_im_conversation_stop_typing_timeout(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_get_typing_timeout:
- * @im: The IM.
- *
- * Returns the IM's typing timeout.
- *
- * Returns: The timeout.
- */
-guint purple_im_conversation_get_typing_timeout(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_set_type_again:
- * @im:  The IM.
- * @val: The number of seconds to wait before allowing another
- *            PURPLE_IM_TYPING message to be sent to the user.  Or 0 to
- *            not send another PURPLE_IM_TYPING message.
- *
- * Sets the quiet-time when no PURPLE_IM_TYPING messages will be sent.
- * Few protocols need this (maybe only MSN).  If the user is still
- * typing after this quiet-period, then another PURPLE_IM_TYPING message
- * will be sent.
- */
-void purple_im_conversation_set_type_again(PurpleIMConversation *im, unsigned int val);
-
-/**
- * purple_im_conversation_get_type_again:
- * @im: The IM.
- *
- * Returns the time after which another PURPLE_IM_TYPING message should be sent.
- *
- * Returns: The time in seconds since the epoch.  Or 0 if no additional
- *         PURPLE_IM_TYPING message should be sent.
- */
-time_t purple_im_conversation_get_type_again(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_start_send_typed_timeout:
- * @im:      The IM.
- *
- * Starts the IM's type again timeout.
- */
-void purple_im_conversation_start_send_typed_timeout(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_stop_send_typed_timeout:
- * @im: The IM.
- *
- * Stops the IM's type again timeout.
- */
-void purple_im_conversation_stop_send_typed_timeout(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_get_send_typed_timeout:
- * @im: The IM.
- *
- * Returns the IM's type again timeout interval.
- *
- * Returns: The type again timeout interval.
- */
-guint purple_im_conversation_get_send_typed_timeout(PurpleIMConversation *im);
-
-/**
- * purple_im_conversation_update_typing:
- * @im: The IM.
- *
- * Updates the visual typing notification for an IM conversation.
- */
-void purple_im_conversation_update_typing(PurpleIMConversation *im);
 
 /**************************************************************************/
 /* Chat Conversation API                                                  */
