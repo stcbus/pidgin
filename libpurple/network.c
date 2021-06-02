dd8ed03684b392c24114dc19f6fde63f5ac84a18
Gary Kramlich <grim@reaperworld.com>
2021-06-01 22:23:58 -0500

    In 2021 we should NOT be enabling UPnP and IP auto discovery by default
    Testing Done:
    Compile only.
    
    Reviewed at https://reviews.imfreedom.org/r/706/
    
    --HG--
    branch : release-2.x.y

--- libpurple/network.c
+++ libpurple/network.c
@@ -1230,9 +1230,9 @@ purple_network_init(void)
 	purple_prefs_add_int	 ("/purple/network/turn_port_tcp", 3478);
 	purple_prefs_add_string("/purple/network/turn_username", "");
 	purple_prefs_add_string("/purple/network/turn_password", "");
-	purple_prefs_add_bool  ("/purple/network/auto_ip", TRUE);
+	purple_prefs_add_bool  ("/purple/network/auto_ip", FALSE);
 	purple_prefs_add_string("/purple/network/public_ip", "");
-	purple_prefs_add_bool  ("/purple/network/map_ports", TRUE);
+	purple_prefs_add_bool  ("/purple/network/map_ports", FALSE);
 	purple_prefs_add_bool  ("/purple/network/ports_range_use", FALSE);
 	purple_prefs_add_int   ("/purple/network/ports_range_start", 1024);
 	purple_prefs_add_int   ("/purple/network/ports_range_end", 2048);
