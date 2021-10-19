0a9aaee29877ce99c22a447b4584b3937d7399f3
Gary Kramlich <grim@reaperworld.com>
2021-10-19 02:52:36 -0500

    Fix the documentation for what does and doesn't need gmodule
    Testing Done:
    Build the docs and verified gmodule was just on finch and that its links worked.
    
    Reviewed at https://reviews.imfreedom.org/r/1078/

--- doc/reference/protocols/facebook/urlmap.js
+++ doc/reference/protocols/facebook/urlmap.js
@@ -5,6 +5,5 @@
 baseURLs = [
     [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
-    [ 'GModule', 'https://docs.gtk.org/gmodule/' ],
     [ 'Purple3', 'https://docs.imfreedom.org/purple3/' ],
 ]
